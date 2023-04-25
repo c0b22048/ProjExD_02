51c579909806abe0e72f195c8153e8f81c5668f6
kota <c0b2204854@edu.teu.ac.jp>
2023-04-25 13:22:21 +0900

    ren2_3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,4 @@
-import pygame as pg
+import pygame as pg #練習問題２までは。vx,vyの場所がわからない
 import sys
 import random
 def main():
@@ -11,9 +11,17 @@ def main():
     tmr = 0
 
     bom_screen = pg.Surface((20,20))    #爆弾の作成
+    vx = 0
+    vy = 0
     bom_screen.set_colorkey(0)
     pg.draw.circle(bom_screen,(255,0,0),(10,10),10)
     bom = bom_screen.get_rect(center=(random.randint(160,1420),random.randint(90,790)))
+
+    kx=0
+    ky=0
+    key_lst = {pg.K_UP:(1),pg.K_DOWN:(-1),pg.K_LEFT:(-1),pg.K_RIGHT:(1)}
+
+
 
 
     while True:
@@ -24,11 +32,13 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bom = bom_screen.get_rect(center=(vx,vy))
         screen.blit(bom_screen,bom)
-
+        vx += 1
+        vy += 1 
+    
         pg.display.update()
         clock.tick(1000)
-
 
 if __name__ == "__main__":
     pg.init()
