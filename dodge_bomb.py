1240acf42f048863a7c4f4fe552f6ad41dc3f83b
kota <c0b2204854@edu.teu.ac.jp>
2023-04-25 14:51:55 +0900

    ren2_4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,16 @@
 import pygame as pg #練習問題２までは。vx,vyの場所がわからない
-import sys
 import random
+import sys
+
+
+delta = {
+    pg.K_UP: (0, -1),  #移動量の上
+    pg.K_DOWN: (0, +1),  #移動量の下
+    pg.K_LEFT: (-1, 0),  #移動量の左
+    pg.K_RIGHT: (+1, 0)  #移動量の右
+        }
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -10,16 +20,18 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     tmr = 0
 
-    bom_screen = pg.Surface((20,20))    #爆弾の作成
+    bom_screen = pg.Surface((20, 20))    #爆弾の作成
     vx = 0
     vy = 0
     bom_screen.set_colorkey(0)
-    pg.draw.circle(bom_screen,(255,0,0),(10,10),10)
-    bom = bom_screen.get_rect(center=(random.randint(160,1420),random.randint(90,790)))
+    pg.draw.circle(bom_screen,(255, 0, 0), (10, 10), 10)
+    bx = random.randint(160, 1420)
+    by = random.randint(90, 790)
+    bb_rect = bom_screen.get_rect()
+    bb_rect.center=bx, by  #爆弾の初期位置をx,yに設定
+    kk_rect=kk_img.get_rect()  #こうかとんをsurfaceからrectに変化
+    kk_rect.center=900, 400  #こうかとんの初期位置の値
 
-    kx=0
-    ky=0
-    key_lst = {pg.K_UP:(1),pg.K_DOWN:(-1),pg.K_LEFT:(-1),pg.K_RIGHT:(1)}
 
 
 
@@ -30,15 +42,29 @@ def main():
                 return 0
 
         tmr += 1
+
+        key_list = pg.key.get_pressed()
+        for k , mv in delta.items():
+            if key_list[k]:
+                kk_rect.move_ip(mv)
+
+
+
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        bom = bom_screen.get_rect(center=(vx,vy))
-        screen.blit(bom_screen,bom)
-        vx += 1
-        vy += 1 
+        screen.blit(kk_img, kk_rect)
+        #bom_rect = bom_screen.get_rect(center=(vx,vy))
+        screen.blit(bom_screen,bb_rect)
+        bb_rect.move_ip(vx, vy)
+        vx, vy = +1, +1
+
+     
     
         pg.display.update()
         clock.tick(1000)
+
+        #if key_lst[pg.K_DOWN]:
+            #kk_img.move_ip(key_lst[pg.K_DOWN])
 
 if __name__ == "__main__":
     pg.init()
